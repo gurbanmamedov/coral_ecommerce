5847d943b60c53957a0cee851ecd97b1f85e39b2
Gurban Mamedov <mike18051996@gmail.com>
2024-06-04 13:53:03 +0400

    added cart/cart functional/cart.js

--- src/js/view/catalogView.js
+++ src/js/view/catalogView.js
@@ -1,56 +1,34 @@
-export function renderProducts(products) {
-  const productLists = document.querySelectorAll(".card__list");
-  productLists.forEach((productList) => {
-    productList.innerHTML = "";
-    products?.slice(0, 8).forEach((product) => {
-      const productItem = document.createElement("li");
-      productItem.classList.add("product");
-      productItem.innerHTML = `
-          <img
-            src="${product?.image}"
-            alt="product image"
-            class="product__image mb-3"
-          />
-          <p class="product__name mb-4">${product?.name}</p>
-          <div class="product__info--bottom d-flex justify-content-between align-items-center">
-            <p class="product__category mb-0">${product?.category}</p>
-            <div class="product__prices align-content-center">
-              <p class="product__price mb-0">$${product?.salePrice}</p>
-            </div>
-          </div>`;
-      productList.appendChild(productItem);
-    });
-  });
-}
+// export const CartView = {
+//   renderCart(cart) {
+//     const cartList = document.getElementById("cartList");
+//     cartList.innerHTML = "";
 
-export function renderFilterButtons(buttonsArray, filterHandler) {
-  const buttonsContainers = document.querySelectorAll(".tabs__list");
-  buttonsContainers.forEach((buttonsContainer) => {
-    buttonsContainer.innerHTML = "";
+//     cart.forEach((item, index) => {
+//       const cartItemHTML = `
+//         <li class="cart-item">
+//           <img src="${item.image}" alt="product image" class="cart-item__image">
+//           <div class="cart-item__details">
+//             <p class="cart-item__name">${item.name}</p>
+//             <p class="cart-item__price">$${item.salePrice.toFixed(2)}</p>
+//           </div>
+//           <button class="cart-item__remove-btn" data-index="${index}">Remove (Index: ${index})</button> <!-- Добавлен вывод индекса для отладки -->
+//         </li>
+//       `;
+//       cartList.innerHTML += cartItemHTML;
+//     });
 
-    let activeButton = null;
+//     const cartTotal = cart.reduce((acc, item) => acc + item.salePrice, 0);
+//     document.querySelector(
+//       ".cart-total"
+//     ).textContent = `Total: $${cartTotal.toFixed(2)}`;
 
-    buttonsArray.forEach((button) => {
-      const buttonItem = document.createElement("li");
-      buttonItem.classList.add("list__items");
-      buttonItem.textContent = button.name;
-      buttonsContainer.appendChild(buttonItem);
-
-      if (button.id === "all") {
-        buttonItem.classList.add("list__items--active");
-        activeButton = buttonItem;
-      }
-
-      buttonItem.addEventListener("click", () => {
-        if (activeButton && activeButton !== buttonItem) {
-          activeButton.classList.remove("list__items--active");
-        }
-
-        buttonItem.classList.add("list__items--active");
-        activeButton = buttonItem;
-
-        filterHandler(button.id);
-      });
-    });
-  });
-}
+//     const removeButtons = document.querySelectorAll(".cart-item__remove-btn");
+//     removeButtons.forEach((button) => {
+//       button.addEventListener("click", () => {
+//         const index = parseInt(button.dataset.index);
+//         console.log("Removing item at index:", index);
+//         CartController.removeFromCart(index);
+//       });
+//     });
+//   },
+// };
